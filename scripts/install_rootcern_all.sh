cc5dc97d1d6eae211fc5527b97e7eaf3d168a492
Firdaus Soberi <firdaussoberi@gmail.com>
2018-10-25 17:31:36 +0800

    Update install_rootcern_all.sh

--- scripts/install_rootcern_all.sh
+++ scripts/install_rootcern_all.sh
@@ -7,9 +7,10 @@ sudo apt-get install wget xorg emacs24 gedit python-pip git dpkg-dev cmake g++ g
 sudo apt-get install gfortran libssl-dev libpcre3-dev xlibmesa-glu-dev libglew1.5-dev libftgl-dev libmysqlclient-dev libfftw3-dev libcfitsio-dev graphviz-dev libavahi-compat-libdnssd-dev libldap2-dev python-dev libxml2-dev libkrb5-dev libgsl0-dev libqt4-dev
 #==========================================================================================
 #=========================Initial variables================================================
+ROOT6VER=6.14.02
 export HOME2=$HOME
 WHERE_TO_DOWNLOAD_ROOT=$HOME2/root_download                                  #can change folder name
-WHERE_TO_INSTALL_ROOT=$HOME2/root_6_14_02                                    #comment if using ROOt ver 5
+WHERE_TO_INSTALL_ROOT=$HOME2/root-${ROOT6VER}_run                            #comment if using ROOt ver 5
                                                                              #can change folder name
 #WHERE_TO_INSTALL_ROOT=$HOME2/root_5_34_36                                   #uncomment if using ROOt ver 5
                                                                              #can change folder name
@@ -18,14 +19,14 @@ WHERE_TO_INSTALL_ROOT=$HOME2/root_6_14_02                                    #co
 sudo mkdir -pv $WHERE_TO_DOWNLOAD_ROOT
 cd $WHERE_TO_DOWNLOAD_ROOT
 #sudo wget https://root.cern.ch/download/root_v5.34.36.source.tar.gz         #uncomment for ROOT ver 5
-sudo wget https://root.cern.ch/download/root_v6.14.02.source.tar.gz          #comment if using ROOt ver 5
+sudo wget https://root.cern.ch/download/root_v${ROOT6VER}.source.tar.gz      #comment if using ROOt ver 5
 #sudo tar -xzvpf root_v5.34.36.source.tar.gz                                 #uncomment for ROOT ver 5
-sudo tar -xzvpf root_v6.14.02.source.tar.gz                                  #comment if using ROOt ver 5
+sudo tar -xzvpf root_v${ROOT6VER}.source.tar.gz                              #comment if using ROOt ver 5
 #===============Building and compiling, works only for source distribution============
 sudo mkdir -pv $WHERE_TO_INSTALL_ROOT
 cd $WHERE_TO_INSTALL_ROOT
 #sudo cmake $WHERE_TO_DOWNLOAD_ROOT/root
-sudo cmake $WHERE_TO_DOWNLOAD_ROOT/root-6.14.02
+sudo cmake $WHERE_TO_DOWNLOAD_ROOT/root-${ROOT6VER}
 #sudo cmake --build . --target VDT
 sudo cmake --build . -- -j2
 #sudo cmake --build . -- -j$(nproc)
