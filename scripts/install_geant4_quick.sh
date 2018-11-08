44982916d47a9f3cbb8638f008a422877a6db378
Firdaus Soberi <firdaussoberi@gmail.com>
2018-11-08 15:59:27 +0800

    Add files via upload

---
+++ scripts/install_geant4_quick.sh
@@ -0,0 +1,8 @@
+echo "GEANT4 auto install script-Firdaus Soberi"
+echo "Enter your password"
+sudo rm -f install_geant4_all.sh*
+echo "Initializing installation of GEANT4"
+sudo wget https://raw.githubusercontent.com/firdaussoberi/cern_stuff/master/scripts/install_geant4_all.sh
+source install_geant4_all.sh
+
+#Link: https://github.com/firdaussoberi/cern_stuff/blob/master/scripts/install_geant4_all.sh
---
+++ scripts/install_rootcern_quick.sh
@@ -0,0 +1,8 @@
+echo "ROOTCERN auto install script-Firdaus Soberi"
+echo "Enter your password"
+sudo rm -f install_rootcern_all.sh*
+echo "Initializing installation of ROOT CERN VER 6"
+sudo wget https://raw.githubusercontent.com/firdaussoberi/cern_stuff/master/scripts/install_rootcern_all.sh
+source install_rootcern_all.sh
+
+#Link: https://github.com/firdaussoberi/cern_stuff/blob/master/scripts/install_rootcern_all.sh
