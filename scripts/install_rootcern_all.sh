4ece4a625922cbe8227a7918b8752b039d758aab
Firdaus Soberi <firdaussoberi@gmail.com>
2018-11-08 16:30:55 +0800

    Update install_rootcern_all.sh

--- scripts/install_rootcern_all.sh
+++ scripts/install_rootcern_all.sh
@@ -8,26 +8,38 @@ sudo apt-get install wget xorg emacs25 emacs24 gedit python-pip git dpkg-dev cma
 sudo apt-get install gfortran libssl-dev libpcre3-dev xlibmesa-glu-dev libglew1.5-dev libftgl-dev libmysqlclient-dev libfftw3-dev libcfitsio-dev graphviz-dev libavahi-compat-libdnssd-dev libldap2-dev python-dev libxml2-dev libkrb5-dev libgsl0-dev libqt4-dev
 #==========================================================================================
 #=========================Initial variables================================================
-ROOT6VER=6.14.02
+ROOTVER=6.14.02
+
 export HOME2=$HOME
-WHERE_TO_DOWNLOAD_ROOT=$HOME2/root_download                                  #can change folder name
-WHERE_TO_INSTALL_ROOT=$HOME2/root-${ROOT6VER}_run                            #comment if using ROOt ver 5
-                                                                             #can change folder name
-#WHERE_TO_INSTALL_ROOT=$HOME2/root_5_34_36                                   #uncomment if using ROOt ver 5
-                                                                             #can change folder name
+#=================================== Checking ROOTVER =====================================
+MAJORROOTVER="${ROOTVER:0:1}"
+if [  $MAJORROOTVER -eq 5 ]
+then
+    ROOTSOURCE_FOLDER = root
+    echo "ROOTVERSION is:" $MAJORROOTVER
+elif [  $MAJORROOTVER -eq 6 ]
+then
+    ROOTSOURCE_FOLDER = root-${ROOTVER}
+    echo "ROOTVERSION is:" $MAJORROOTVER
+else
+    echo "ERROR: ROOTVERSION not found. Check the install_rootcern_all.sh file and start again"
+    return
+fi
+#==========================================================================================
+WHERE_TO_DOWNLOAD_ROOT=$HOME2/root_download                                 #can change folder name
+WHERE_TO_INSTALL_ROOT=$HOME2/root-${ROOTVER}_run                            #can change folder name 
+
 #=======================================================================================
 #========================Download files================================================
 sudo mkdir -pv $WHERE_TO_DOWNLOAD_ROOT
 cd $WHERE_TO_DOWNLOAD_ROOT
-#sudo wget https://root.cern.ch/download/root_v5.34.36.source.tar.gz         #uncomment for ROOT ver 5
-sudo wget https://root.cern.ch/download/root_v${ROOT6VER}.source.tar.gz      #comment if using ROOt ver 5
-#sudo tar -xzvpf root_v5.34.36.source.tar.gz                                 #uncomment for ROOT ver 5
-sudo tar -xzvpf root_v${ROOT6VER}.source.tar.gz                              #comment if using ROOt ver 5
+sudo wget https://root.cern.ch/download/root_v${ROOTVER}.source.tar.gz      
+sudo tar -xzvpf root_v${ROOTVER}.source.tar.gz                              #comment if using ROOt ver 5
+
 #===============Building and compiling, works only for source distribution============
 sudo mkdir -pv $WHERE_TO_INSTALL_ROOT
 cd $WHERE_TO_INSTALL_ROOT
-#sudo cmake $WHERE_TO_DOWNLOAD_ROOT/root
-sudo cmake $WHERE_TO_DOWNLOAD_ROOT/root-${ROOT6VER}
+sudo cmake $WHERE_TO_DOWNLOAD_ROOT/${ROOTSOURCE_FOLDER}
 sudo cmake --build . -- -j2
 #sudo cmake --build . -- -j$(nproc)
 #nproc is for how many core the cpu has, this is for faster building by using all cores
